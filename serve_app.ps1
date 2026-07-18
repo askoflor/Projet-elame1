26a53881a304f55d6c80caf01f2100d7d45404a6
ASSAKO FLORENT JUNIOR <parfaitassako@gmail.com>
2026-07-18 19:02:49 +0100

    Clear main branch before adding new clean structure

--- serve_app.ps1
+++
@@ -1,5 +0,0 @@
-$port = $args[0]
-if (-not $port) { $port = 8080 }
-$dir = "C:\Users\ngounou tomy\Downloads\frontend-tmp\build\web"
-Set-Location -LiteralPath $dir
-npx serve . -p $port --no-clipboard
