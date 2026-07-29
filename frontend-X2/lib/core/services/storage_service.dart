6d0883da080f8cb4d7cb010afae94a59f52be062
ASSAKO FLORENT JUNIOR <parfaitassako@gmail.com>
2026-07-29 17:11:30 +0100

    Modification configuration du module d'inscription

--- frontend-X2/lib/core/services/storage_service.dart
+++ frontend-X2/lib/core/services/storage_service.dart
@@ -1,36 +1,44 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../constants/app_constants.dart';
 
+/// Stockage local des jetons d'authentification.
+///
+/// Utilise shared_preferences plutot que flutter_secure_storage : sur le web,
+/// flutter_secure_storage s'appuie sur l'API Web Crypto du navigateur, qui
+/// n'est disponible que dans un contexte securise (HTTPS ou localhost). Tant
+/// que l'app est servie en HTTP simple (IP sans certificat), cette API est
+/// indisponible et fait echouer le stockage. shared_preferences (localStorage
+/// sur le web) fonctionne quel que soit le contexte.
 class StorageService {
-  static const _storage = FlutterSecureStorage(
-    webOptions: WebOptions(
-      dbName: 'nzela_service_storage',
-      publicKey: 'nzela_service_key',
-    ),
-  );
-
   static Future<void> saveToken(String token) async {
-    await _storage.write(key: AppConstants.tokenKey, value: token);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(AppConstants.tokenKey, token);
   }
 
   static Future<String?> getToken() async {
-    return await _storage.read(key: AppConstants.tokenKey);
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(AppConstants.tokenKey);
   }
 
   static Future<void> deleteToken() async {
-    await _storage.delete(key: AppConstants.tokenKey);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(AppConstants.tokenKey);
   }
 
   static Future<bool> hasToken() async {
-    final token = await _storage.read(key: AppConstants.tokenKey);
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString(AppConstants.tokenKey);
     return token != null && token.isNotEmpty;
   }
 
   static Future<void> saveRefreshToken(String token) async {
-    await _storage.write(key: AppConstants.refreshTokenKey, value: token);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(AppConstants.refreshTokenKey, token);
   }
 
   static Future<void> clearAll() async {
-    await _storage.deleteAll();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(AppConstants.tokenKey);
+    await prefs.remove(AppConstants.refreshTokenKey);
   }
 }
